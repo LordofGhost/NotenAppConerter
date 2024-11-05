86338d632c99d2bd91cec973f0902125c9250b0f
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-11-05 15:37:31 +0100

    create main methode

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String in = "Hallo, anbei meine Prüfungen und Noten: Datum, Fach, Prüfungsart, Note 11.11.1111, IT , Stegreifaufgabe, 1 12.12.1212, Englisch , Schulaufgabe , 1 Bei Fragen kommen Sie gerne auf mich zu. Beste Grüße Diese Nachricht wurde aus Notenapp - Schulplaner digital versendet. Erfahre mehr unter www.notenapp.com";
+        AnalyzeInput.getExamsFromString(in);
+    }
+}
